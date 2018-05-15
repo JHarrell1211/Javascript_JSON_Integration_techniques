32e1f7ada539ed80da8d55af4eba00bc97043873
JHarrell1211 <julian1211@gmail.com>
2018-05-14 22:15:29 -0400

    first commit

---
+++ WebApplication1/JavaScript.js
@@ -0,0 +1,24 @@
+﻿//OBJECTS
+var info = {
+    "full_name": "Ray Villalobos",
+    "title": "Staff Author",
+    "qty": 3,
+    //creating array 
+    "courses": [
+        "JavaScript & JSON",
+        "Facebook Apps",
+        "jQuery Mobile Web Apps"
+    ],
+    "links": {
+        "blog": "http://iviewsource.com",
+        "facebook": "http://facebook.com/iviewsource",
+        "youtube": "http://www.youtube.com/planetoftheweb",
+        "podcast": "http://feeds.feedburner.com/authoredcontent",
+        "twitter": "http://twitter.com/planetoftheweb"
+    }
+};
+console.log(info.full_name);
+console.log(info.courses[1]);
+console.log(info.links.youtube);
+
+
